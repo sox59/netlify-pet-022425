f21645aa57917ee07a586dd5b62facc858533ed7
Paul <ppagliughi@yahoo.com>
2025-02-25 05:45:26 -0600

    2nd commit

--- netlify/functions/pets.js
+++ netlify/functions/pets.js
@@ -1,7 +1,7 @@
 const handler = async () => {
   return {
     statusCode: 200,
-    headers: { "Content-Type": "application/plain" },
+    headers: { "Content-Type": "text/plain" },
     body: "paul".toUpperCase()
   }
 }
