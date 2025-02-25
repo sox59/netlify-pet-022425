2d3bddfe8fa905a49e3ab7a05795455b71df2426
Paul <ppagliughi@yahoo.com>
2025-02-25 06:09:56 -0600

    3rd commit

--- netlify/functions/pets.js
+++ netlify/functions/pets.js
@@ -2,7 +2,7 @@ const handler = async () => {
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/plain" },
-    body: "paul".toUpperCase()
+    body: "<h1>Hello this is a headline</h1>"
   }
 }
 
