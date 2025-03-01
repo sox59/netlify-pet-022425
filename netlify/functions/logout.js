19ab3ddd5552edc7b7156111246c3c5d333b01c0
Paul <ppagliughi@yahoo.com>
2025-03-01 06:57:18 -0600

    Finished login and logout 1

--- netlify/functions/logout.js
+++ netlify/functions/logout.js
@@ -3,39 +3,35 @@ const cookie = require("cookie")
 
 const handler = async event => {
 
-  const body = JSON.parse(event.body)
-
-
-
-  if (body.username == "wrigley" && body.password == "field") {
-
-    const myCookie = cookie.serialize("petadoption", "-", {
-
-      httpOnly: true,
-      path: "/",
-      sameSite: "strict",
-      maxAge: 0
 
 
 
 
 
-    })
 
-    return {
-      statusCode: 200,
-      headers: { "Content-Type": "application/json", "Set-Cookie": myCookie, "Location": "/" },
-      body: JSON.stringify({ success: true })
-    }
+  const myCookie = cookie.serialize("petadoption", "-", {
+
+    httpOnly: true,
+    path: "/",
+    sameSite: "strict",
+    maxAge: 0
 
 
-  }
+
+
+
+  })
 
   return {
     statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ success: false })
+    headers: { "Content-Type": "application/json", "Set-Cookie": myCookie, "Location": "/" },
+    body: JSON.stringify({ success: true })
   }
+
+
 }
+
+
+
 
 module.exports = { handler }
