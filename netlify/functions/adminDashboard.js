2fae762d04dad67b5dde55d56189239170cfdf77
Paul <ppagliughi@yahoo.com>
2025-03-02 05:53:13 -0600

    Styled admin and login 2

--- netlify/functions/adminDashboard.js
+++ netlify/functions/adminDashboard.js
@@ -1,5 +1,6 @@
 
 const cookie = require("cookie")
+const { MongoClient } = require("mongodb")
 
 const handler = async event => {
 
@@ -7,10 +8,18 @@ const handler = async event => {
 
   if (incomingCookie?.petadoption == "hgreuf09ruiojvnmnj9090ruhvisjkiuhourhouhhguhenvkj") {
 
+    const client = new MongoClient(process.env.CONNECTIONSTRING)
+    await client.connect()
+
+    const pets = await client.db().collection("pets").find().toArray()
+    client.close()
+
+    const petsHTML = generateHTML(pets)
+
     return {
       statusCode: 200,
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ success: true })
+      body: JSON.stringify({ success: true, pets: petsHTML })
     }
 
 
@@ -21,6 +30,32 @@ const handler = async event => {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ success: false })
   }
+}
+
+function generateHTML(pets) {
+
+  let outHTML = `<div class="list-of-pets">`
+
+  outHTML += pets.map(pet => {
+
+    return ` <div class="pet-card">
+      <div class="pet-card-text">
+        <h3>${pet.name}</h3>
+        <p class="pet-description">${pet.description}</p>
+      </div>
+      <div class="pet-card-photo">
+        <img src="/images/fallback.jpg" alt="A ${pet.species} named ${pet.name}.">
+      </div>
+    </div> `
+
+  }).join("")
+
+  outHTML += "</div>"
+
+  return outHTML
+
+
+
 }
 
 module.exports = { handler }
