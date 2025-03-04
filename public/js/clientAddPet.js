5780b63dc72579443defe4ec796085f46128d8c3
Paul <ppagliughi@yahoo.com>
2025-03-04 04:30:46 -0600

    Add new pet form 1

--- public/js/clientAddPet.js
+++ public/js/clientAddPet.js
@@ -1,4 +1,4 @@
-document.querySelector("#add-new-pet-form").addEventListener("submit", function (e) {
+document.querySelector("#add-new-pet-form").addEventListener("submit", async function (e) {
   e.preventDefault()
 
   const pet = {
@@ -10,6 +10,18 @@ document.querySelector("#add-new-pet-form").addEventListener("submit", function 
 
   }
 
-  console.log(pet)
+
+  const ourPromise = await fetch("/.netlify/functions/addPet", {
+
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    boby: JSON.stringify(pet)
+
+
+  })
+
+  const theResponse = await ourPromise.json()
+  window.location = "/admin"
+
 
 })
