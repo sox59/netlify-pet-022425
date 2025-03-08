501a1c6b92bb9a53d7c3e31cfa66e0be81a3c62a
Paul <ppagliughi@yahoo.com>
2025-03-08 05:48:38 -0600

    Finished Edit feature

---
+++ netlify/functions/saveChanges.js
@@ -0,0 +1,89 @@
+const sanitizeHTML = require("sanitize-html")
+const getDbClient = require("../../our-library/getDbClient")
+const isAdmin = require("../../our-library/isAdmin")
+const { ObjectId } = require("mongodb")
+
+function cleanUp(x) {
+
+  return sanitizeHTML(x, {
+
+    allowedTags: [],
+    allowedAttributes: {}
+
+  })
+
+}
+
+const handler = async event => {
+
+  const body = JSON.parse(event.body)
+
+  // data validation and sanitation
+
+  let pet = {
+
+    name: body.name,
+    species: cleanUp(body.species),
+    description: cleanUp(body.description),
+    birthYear: new Date().getFullYear()
+
+  }
+
+  if (body.birthYear > 999 && body.birthYear < 9999) {
+    pet.birthYear = body.birthYear
+
+  }
+
+  if (pet.species != "cat" && pet.species != "dog") {
+    pet.species = "dog"
+
+  }
+
+
+
+
+
+  if (isAdmin(event)) {
+    // save into database
+
+    if (!ObjectId.isValid(body.id)) {
+
+      return {
+
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ success: false })
+
+      }
+
+
+    }
+
+
+
+    const client = await getDbClient()
+    await client.db().collection("pets").findOneAndUpdate({ _id: ObjectId.createFromHexString(body.id) }, { $set: pet })
+    client.close()
+
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ success: true })
+    }
+
+
+  }
+
+  // no permission
+
+
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ success: false })
+  }
+
+
+}
+
+module.exports = { handler }
--- public/js/clientEditPet.js
+++ public/js/clientEditPet.js
@@ -39,3 +39,42 @@ async function getEditPet() {
 
 getEditPet()
 
+//****************************************************************************************************************** 
+
+document.querySelector("#edit-pet-form").addEventListener("submit", async function (e) {
+  e.preventDefault()
+
+  const pet = {
+    id,
+    name: document.querySelector("#name").value,
+    birthYear: document.querySelector("#birthYear").value,
+    species: document.querySelector("#species").value,
+    description: document.querySelector("#description").value
+
+  }
+
+
+  const ourPromise = await fetch("/.netlify/functions/saveChanges", {
+
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(pet)
+
+
+  })
+
+  const theResponse = await ourPromise.json()
+
+  if (theResponse.success) {
+
+    window.location = "/admin"
+
+  }
+
+
+
+
+
+
+})
+
